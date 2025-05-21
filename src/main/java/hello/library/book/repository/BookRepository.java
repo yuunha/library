a8bd585f920598c01995cf68994a47effafbbef2
yuunha <h000414@naver.com>
2025-05-21 12:31:57 +0900

    refactor : repositoy 안 쓰는 메소드 삭제

--- src/main/java/hello/library/book/repository/BookRepository.java
+++ src/main/java/hello/library/book/repository/BookRepository.java
@@ -10,12 +10,6 @@ import jakarta.validation.constraints.NotBlank;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-	// ISBN으로 책을 찾는 메서드
-	Optional<Book> findByIsbn(String isbn);
-
-	// 카테고리로 책을 찾는 메서드 (필요 시)
-	List<Book> findByCategory(String category);
-
 	List<Book> findByBookNameContainingIgnoreCase(String keyword); // 제목 키워드로 검색
 
 	boolean existsByIsbn(@NotBlank(message = "ISBN은 필수입니다.") String isbn);
